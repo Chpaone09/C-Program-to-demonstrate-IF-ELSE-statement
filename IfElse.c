8205bef35a5d0fa6e936f451fe1a754605e2fff3
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-11 22:11:04 +0530

    Program to demonstrate IF-ELSE Statement in c

---
+++ IfElse.c
@@ -0,0 +1,21 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate IF-ELSE Statement in c.
+Author  : Chpaone09®
+Date    : Oct 11, 2020
+
+************************************************************
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+    int a = 5;
+    if((a%2)==0){
+        printf("\n\n\t %d is EVEN", a);
+    }else{
+        printf("\n\n\t %d is ODD", a); 
+    }
+}
